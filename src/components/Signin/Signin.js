c68322f0560b6b2093cf1bcc297cd602a4ac775b
Veneta Stefanova <venetastefanovaa@gmail.com>
2019-01-13 15:42:10 +0200

    session management implementation and redirecting to the user profile

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,6 +18,11 @@ class Signin extends React.Component {
     this.setState({signInPassword: event.target.value})
   }
 
+  //saving the token into session
+  saveAuthTokenInSession = (token) => {
+    window.sessionStorage.setItem('token', token);
+  }
+
   onSubmitSignIn = () => {
     fetch('http://localhost:3000/signin', {
       method: 'post',
@@ -29,9 +34,23 @@ class Signin extends React.Component {
     })
       .then(response => response.json())
       .then(data => {
-        if (data.userId) {
-          this.props.loadUser(data)
-          this.props.onRouteChange('home');
+        if (data.userId && data.success === 'true') {
+          this.saveAuthTokenInSession(data.token);
+          fetch(`http://localhost:3000/profile/${data.userId}`, {
+            method: 'get',
+            headers: {
+              'Content-Type' : 'application/json',
+              'Authorization': data.token
+            }
+          })
+          .then(resp=>resp.json())
+          .then(user=> {
+            if(user && user.email){
+              this.props.loadUser(user);
+              this.props.onRouteChange('home');
+            }
+          })
+          
         }
       })
   }
